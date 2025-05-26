d9e10091986132c3098a8a07aa3118a29cc47462
Edwin Gonzalez <37126807+edgonza12@users.noreply.github.com>
2025-05-26 14:50:43 -0600

    Cambios 2

--- Tools/osint_investigator/views.py
+++ Tools/osint_investigator/views.py
@@ -1,40 +1,11 @@
-# osint_investigator/views.py
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from .forms import InvestigationForm
-from .models import InvestigationHistory
-from .sherlock_wrapper import run_sherlock  # Importa la función wrapper de Sherlock
+from .utils import run_sherlock
 
-@login_required
 def investigator_view(request):
-    form = InvestigationForm()
-    sherlock_results = {}
-    investigation = None
-
+    results = None
     if request.method == 'POST':
-        form = InvestigationForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            email = form.cleaned_data.get('email')
-            full_name = form.cleaned_data.get('full_name')
-
-            # Ejecutar Sherlock si se proporciona un nombre de usuario
-            if username:
-                sherlock_results = run_sherlock(username)
-
-            # Guardar la investigación en historial
-            investigation = InvestigationHistory.objects.create(
-                user=request.user,
-                username=username,
-                email=email,
-                full_name=full_name,
-                result_summary="Búsqueda completada con Sherlock" if username else "Búsqueda sin nombre de usuario"
-            )
-
-            # Puedes almacenar resultados adicionales en el historial si quieres (otro paso)
-
-    return render(request, 'osint_investigator/investigation_results.html', {
-        'form': form,
-        'sherlock_results': sherlock_results,
-        'investigation': investigation,
-    })
+        username = request.POST.get('username')
+        if username:
+            output = run_sherlock(username)
+            results = output.splitlines()
+    return render(request, 'investigator.html', {'results': results})
