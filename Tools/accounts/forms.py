fe28cbc4ab390f60ac9a89869bb184a8ef7d7826
Edwin Gonzalez <37126807+edgonza12@users.noreply.github.com>
2025-12-04 12:29:30 -0600

    Ultimos Cambios

--- Tools/accounts/forms.py
+++ Tools/accounts/forms.py
@@ -20,4 +20,4 @@ class CustomAuthenticationForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(CustomAuthenticationForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password'].widget.attrs.update({'class': 'form-control'})
+        self.field s['password'].widget.attrs.update({'class': 'form-control'})
